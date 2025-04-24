23dfdd52e73acb12bb636ffb38996585f96d729e
alvyae <alfiaict@gmail.com>
2025-04-24 11:28:09 +0700

    alfiaXBsite

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🅰️lfia's🗯")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+    "latihan membuat website di github dan streamlit"
